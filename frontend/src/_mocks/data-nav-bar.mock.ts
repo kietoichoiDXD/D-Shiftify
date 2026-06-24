84c7be1ad1b1b81697783d813895d680f63b1aae
Face Attendance System <attendance@system.local>
2026-06-24 17:30:43 +0700

    chore(frontend): remove mock and test directories

--- frontend/src/_mocks/data-nav-bar.mock.ts
+++
@@ -1,5 +0,0 @@
-export const navLinks = [
-  { labelKey: 'features.title', to: '#features' },
-  { labelKey: 'techStack.title', to: '#tech-stack' },
-  { labelKey: 'gettingStarted.title', to: '#getting-started' }
-]
